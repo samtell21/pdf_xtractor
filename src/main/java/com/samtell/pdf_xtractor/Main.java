956c7adc391ab0e1c27d024510fdc41409986d4b
Sam <samtell21@gmail.com>
2020-08-12 15:16:36 -0400

    initial commit

---
+++ src/main/java/com/samtell/pdf_xtractor/Main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.samtell.pdf_xtractor;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author samtell
+ */
+public class Main {
+    public static void main(String[] args)
+            throws IOException
+    {
+        MainGui.main(args);
+    }
+
+    
+}
+
+    
