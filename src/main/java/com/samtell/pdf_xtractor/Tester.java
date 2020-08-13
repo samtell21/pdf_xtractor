58d02905852b37447ccc8db549492ecafea47a34
Sam <samtell21@gmail.com>
2020-08-13 14:54:03 -0400

    netbeans doesn't know best practice

--- src/main/java/com/samtell/pdf_xtractor/Tester.java
+++ src/main/java/com/samtell/pdf_xtractor/Tester.java
@@ -107,23 +107,18 @@ public class Tester extends javax.swing.JFrame {
 		    break;
 		}
 	    }
-	} catch (ClassNotFoundException ex) {
-	    java.util.logging.Logger.getLogger(Tester.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-	} catch (InstantiationException ex) {
-	    java.util.logging.Logger.getLogger(Tester.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-	} catch (IllegalAccessException ex) {
-	    java.util.logging.Logger.getLogger(Tester.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-	} catch (javax.swing.UnsupportedLookAndFeelException ex) {
+	} catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
 	    java.util.logging.Logger.getLogger(Tester.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
 	}
 	//</editor-fold>
 	//</editor-fold>
+	
+	//</editor-fold>
+	//</editor-fold>
 
 	/* Create and display the form */
-	java.awt.EventQueue.invokeLater(new Runnable() {
-	    public void run() {
-		new Tester().setVisible(true);
-	    }
+	java.awt.EventQueue.invokeLater(() -> {
+	    new Tester().setVisible(true);
 	});
     }
     
