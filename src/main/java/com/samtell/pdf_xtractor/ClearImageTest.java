a8540fa2be2db3ab6bb3ac3c36b76bdeb09b4145
Sam <samtell21@gmail.com>
2020-08-18 09:21:11 -0400

    clearimage init

---
+++ src/main/java/com/samtell/pdf_xtractor/ClearImageTest.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.samtell.pdf_xtractor;
+
+import ClearImageJNI.ICiServer;
+
+/**
+ *
+ * @author samtell
+ */
+public class ClearImageTest {
+    
+    ICiServer initClearImage(){
+	
+    }
+    
+}
